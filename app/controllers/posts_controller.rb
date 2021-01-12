a39c9852f1ceabc0b7cef08ce1510b165fbf294e
ChrisMarlier <marlier.christopher@gmail.com>
2021-01-12 22:29:38 +0100

    Verify Firebase token

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+	before_action :firebase_verification#, except: :create
+
+
+	def index
+		render :json => {:response => 'You did it' },:status => 200
+	end
+
+end
